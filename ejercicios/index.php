4f8edb06e95b46686ee67d02539ee54f2ddd1a8e
Andrea Solís Tejada <dreylizz25@gmail.com>
2022-04-03 17:55:10 +0200

    Array países completo

---
+++ ejercicios/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Andrea Solís Tejada</title>
+</head>
+<body>
+    <h1>Ejercicios Servidor</h1>
+    <h2>Enlace al repositorio completo de github: </h2>
+    <a href="https://github.com/a19sotean/Servidor" target="_blank">Repositorio</a>
+    <h2>Unidad 2</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad2" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 3 Arrays</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad3Arrays" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 3 Formularios</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad3Formularios" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 3 Funciones</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad3Funciones" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 3 Tanda 1</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad3Tanda1" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 3 Tanda 2</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad3Tanda2" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 4 Cookies</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad4Cookies" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 4 Errores</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad4Errores" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 4 Ficheros</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad4Ficheros" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 4 Sesiones</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad4Sesiones" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 5 POO</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad5POO" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 6 Bases de datos</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad6BasesDatos" target="_blank">Ir al repositorio</a>
+    <h2>Unidad 8</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/Unidad8" target="_blank">Ir al repositorio</a>
+    <h2>Buscaminas</h2>
+    <a href="https://github.com/a19sotean/buscaminasPHP" target="_blank">Ir al repositorio</a>
+    <h2>Calendario</h2>
+    <a href="https://github.com/a19sotean/calendarioPHP" target="_blank">Ir al repositorio</a>
+    <h2>Superhéroes</h2>
+    <a href="https://github.com/a19sotean/Servidor/tree/main/sh/superheros" target="_blank">Ir al repositorio</a>
+</body>
+</html>
